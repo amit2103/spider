af357a68a90cf3ecbf5697f3f45960fb89013d81
amit2103 <amit.pandey2103@gmail.com>
2015-02-01 23:20:36 +0530

    modified gitignore

--- src/main/java/org/spider/lambda/Sequence.java
+++ src/main/java/org/spider/lambda/Sequence.java
@@ -86,59 +86,28 @@ public interface Sequence<T> extends Stream<T>, Iterable<T> {
         return foldLeft(this, seed, function);
     }
 
-    /**
-     * Fold a Stream to the right.
-     * <p>
-     * <code><pre>
-     * // "cba"
-     * Seq.of("a", "b", "c").foldRight("", (t, u) -> u + t)
-     * </pre></code>
-     */
     default <U> U foldRight(U seed, BiFunction<? super T, U, U> function) {
         return foldRight(this, seed, function);
     }
 
 
-    /**
-     * Reverse a stream.
-     * <p>
-     * <code><pre>
-     * // (3, 2, 1)
-     * Seq.of(1, 2, 3).reverse()
-     * </pre></code>
-     */
     default Sequence<T> reverse() {
         return reverse(this);
     }
 
 
-    /**
-     * Returns a stream with all elements skipped for which a predicate evaluates to <code>true</code>.
-     * @see #skipWhile(Stream, Predicate)
-     */
     default Sequence<T> skipWhile(Predicate<? super T> predicate) {
         return skipWhile(this, predicate);
     }
 
-    /**
-     * Returns a stream with all elements skipped for which a predicate evaluates to <code>false</code>.
-     * @see #skipUntil(Stream, Predicate)
-     */
     default Sequence<T> skipUntil(Predicate<? super T> predicate) {
         return skipUntil(this, predicate);
     }
 
-    /**
-     * Returns a stream limited to all elements for which a predicate evaluates to <code>true</code>.
-     */
     default Sequence<T> getWhile(Predicate<? super T> predicate) {
         return limitWhile(this, predicate);
     }
 
-    /**
-     * Returns a stream limited to all elements for which a predicate evaluates to false
-     * @see #limitUntil(Stream, Predicate)
-     */
     default Sequence<T> getUntil(Predicate<? super T> predicate) {
         return limitUntil(this, predicate);
     }
@@ -149,16 +118,6 @@ public interface Sequence<T> extends Stream<T>, Iterable<T> {
     }
 
 
-    /**
-     * Split a stream at the head.
-     * <p>
-     * <code><pre>
-     * // tuple(1, (2, 3, 4, 5, 6))
-     * Seq.of(1, 2, 3, 4, 5, 6).splitHead(3)
-     * </pre></code>
-     *
-     * @see #splitAt(Stream, long)
-     */
     default Tuple2<Optional<T>, Sequence<T>> splitAtHead() {
         return splitAtHead(this);
     }
@@ -183,240 +142,118 @@ public interface Sequence<T> extends Stream<T>, Iterable<T> {
     }
 
 
-    /**
-     * Returns a limited interval from a given Stream.
-     * <p>
-     * <code><pre>
-     * // (4, 5)
-     * Seq.of(1, 2, 3, 4, 5, 6).slice(3, 5)
-     * </pre></code>
-     *
-     * @see #slice(Stream, long, long)
-     */
     default Sequence<T> slice(long from, long to) {
         return slice(this, from, to);
     }
 
-    /**
-     * Collect a Stream into a Collection.
-     *
-     * @see #toCollection(Stream, Supplier)
-     */
     default <C extends Collection<T>> C toCollection(Supplier<C> collectionFactory) {
         return toCollection(this, collectionFactory);
     }
 
-    /**
-     * Collect a Stream into a List.
-     *
-     * @see #toList(Stream)
-     */
     default List<T> toList() {
         return toList(this);
     }
-
-    /**
-     * Collect a Stream into a Set.
-     *
-     * @see #toSet(Stream)
-     */
+    
     default Set<T> toSet() {
         return toSet(this);
     }
 
-    /**
-     * Collect a Stream into a Map.
-     *
-     * @see #toMap(Stream, Function, Function)
-     */
+    
     default <K, V> Map<K, V> toMap(Function<T, K> keyMapper, Function<T, V> valueMapper) {
         return toMap(this, keyMapper, valueMapper);
     }
 
-    /**
-     * Consume a stream and concatenate all elements using a separator.
-     */
     default String toString(String separator) {
         return toString(this, separator);
     }
 
-    /**
-     * Get the maximum value by a function.
-     */
     default <U extends Comparable<U>> Optional<T> minBy(Function<T, U> function) {
         return minBy(function, naturalOrder());
     }
-
-    /**
-     * Get the maximum value by a function.
-     */
+    
     default <U> Optional<T> minBy(Function<T, U> function, Comparator<? super U> comparator) {
         return map(t -> tuple(t, function.apply(t)))
               .min(comparing(Tuple2::v2, comparator))
               .map(t -> t.v1);
     }
 
-    /**
-     * Get the maximum value by a function.
-     */
     default <U extends Comparable<U>> Optional<T> maxBy(Function<T, U> function) {
         return maxBy(function, naturalOrder());
     }
 
-    /**
-     * Get the maximum value by a function.
-     */
+    
     default <U> Optional<T> maxBy(Function<T, U> function, Comparator<? super U> comparator) {
         return map(t -> tuple(t, function.apply(t)))
               .max(comparing(Tuple2::v2, comparator))
               .map(t -> t.v1);
     }
 
-    // Methods taken from LINQ
-    // -----------------------
-
-    /**
-     * Keep only those elements in a stream that are of a given type.
-     * <p>
-     * <code><pre>
-     * // (1, 2, 3)
-     * Seq.of(1, "a", 2, "b", 3).ofType(Integer.class)
-     * </pre></code>
-     *
-     * @see #ofType(Stream, Class)
-     */
+   
     default <U> Sequence<U> ofType(Class<U> type) {
         return ofType(this, type);
     }
 
-    /**
-     * Cast all elements in a stream to a given type, possibly throwing a {@link ClassCastException}.
-     * <p>
-     * <code><pre>
-     * // ClassCastException
-     * Seq.of(1, "a", 2, "b", 3).cast(Integer.class)
-     * </pre></code>
-     *
-     * @see #cast(Stream, Class)
-     */
     default <U> Sequence<U> cast(Class<U> type) {
         return cast(this, type);
     }
 
-    // Shortcuts to Collectors
-    // -----------------------
-
-    /**
-     * Shortcut for calling {@link Stream#collect(Collector)} with a
-     * {@link Collectors#groupingBy(Function)} collector.
-     */
     default <K> Map<K, List<T>> groupBy(Function<? super T, ? extends K> classifier) {
         return collect(Collectors.groupingBy(classifier));
     }
 
-    /**
-     * Shortcut for calling {@link Stream#collect(Collector)} with a
-     * {@link Collectors#groupingBy(Function, Collector)} collector.
-     */
     default <K, A, D> Map<K, D> groupBy(Function<? super T, ? extends K> classifier, Collector<? super T, A, D> downstream) {
         return collect(Collectors.groupingBy(classifier, downstream));
     }
 
-    /**
-     * Shortcut for calling {@link Stream#collect(Collector)} with a
-     * {@link Collectors#groupingBy(Function, Supplier, Collector)} collector.
-     */
     default <K, D, A, M extends Map<K, D>> M groupBy(Function<? super T, ? extends K> classifier, Supplier<M> mapFactory, Collector<? super T, A, D> downstream) {
         return collect(Collectors.groupingBy(classifier, mapFactory, downstream));
     }
 
-    /**
-     * Shortcut for calling {@link Stream#collect(Collector)} with a
-     * {@link Collectors#joining()}
-     * collector.
-     */
     default String join() {
         return map(Objects::toString).collect(Collectors.joining());
     }
 
-    /**
-     * Shortcut for calling {@link Stream#collect(Collector)} with a
-     * {@link Collectors#joining(CharSequence)}
-     * collector.
-     */
     default String join(CharSequence delimiter) {
         return map(Objects::toString).collect(Collectors.joining(delimiter));
     }
 
-    /**
-     * Shortcut for calling {@link Stream#collect(Collector)} with a
-     * {@link Collectors#joining(CharSequence, CharSequence, CharSequence)}
-     * collector.
-     */
     default String join(CharSequence delimiter, CharSequence prefix, CharSequence suffix) {
         return map(Objects::toString).collect(Collectors.joining(delimiter, prefix, suffix));
     }
 
-    /**
-     * @see Stream#of(Object)
-     */
     static <T> Sequence<T> of(T value) {
         return sequence(Stream.of(value));
     }
 
-    /**
-     * @see Stream#of(Object)
-     */
     static <T> Sequence<T> of(Stream<T> stream) {
         return sequence(stream);
     }
 
-    /**
-     * @see Stream#of(Object[])
-     */
     @SafeVarargs
     static <T> Sequence<T> of(T... values) {
         return sequence(Stream.of(values));
     }
 
-    /**
-     * @see Stream#empty()
-     */
     static <T> Sequence<T> empty() {
         return sequence(Stream.empty());
     }
 
-    /**
-     * @see Stream#iterate(Object, UnaryOperator)
-     */
     static <T> Sequence<T> iterate(final T seed, final UnaryOperator<T> f) {
         return sequence(Stream.iterate(seed, f));
     }
 
-    /**
-     * @see Stream#generate(Supplier)
-     */
     static Sequence<Void> generate() {
         return generate(() -> null);
     }
 
-    /**
-     * @see Stream#generate(Supplier)
-     */
     static <T> Sequence<T> generate(T value) {
         return generate(() -> value);
     }
-
-    /**
-     * @see Stream#generate(Supplier)
-     */
+    
     static <T> Sequence<T> generate(Supplier<T> s) {
         return sequence(Stream.generate(s));
     }
 
-    /**
-     * Wrap a Stream into a Seq.
-     */
     static <T> Sequence<T> sequence(Stream<T> stream) {
         if (stream instanceof Sequence)
             return (Sequence<T>) stream;
@@ -424,35 +261,21 @@ public interface Sequence<T> extends Stream<T>, Iterable<T> {
         return new SequenceImpl<>(stream);
     }
 
-    /**
-     * Wrap an Iterable into a sequence.
-     */
     static <T> Sequence<T> sequence(Iterable<T> iterable) {
         return sequence(iterable.iterator());
     }
 
     /**
-     * Wrap an Iterator into a Seq.
+     * Convert an Iterator to a Sequence.
      */
     static <T> Sequence<T> sequence(Iterator<T> iterator) {
         return sequence(StreamSupport.stream(spliteratorUnknownSize(iterator, ORDERED), false));
     }
 
-    /**
-     * Wrap an Optional into a Seq.
-     */
     static <T> Sequence<T> sequence(Optional<T> optional) {
         return optional.map(Sequence::of).orElseGet(Sequence::empty);
     }
 
-    /**
-     * Repeat a stream infinitely.
-     * <p>
-     * <code><pre>
-     * // (1, 2, 3, 1, 2, 3, ...)
-     * Seq.of(1, 2, 3).cycle();
-     * </pre></code>
-     */
     static <T> Sequence<T> cycle(Stream<T> stream) {
         final List<T> list = new ArrayList<>();
 
@@ -490,26 +313,10 @@ public interface Sequence<T> extends Stream<T>, Iterable<T> {
     }
 
 
-    /**
-     * Zip two streams into one.
-     * <p>
-     * <code><pre>
-     * // (tuple(1, "a"), tuple(2, "b"), tuple(3, "c"))
-     * Sequence.of(1, 2, 3).zip(Sequence.of("a", "b", "c"))
-     * </pre></code>
-     */
     static <T1, T2> Sequence<Tuple2<T1, T2>> zip(Stream<T1> left, Stream<T2> right) {
         return zip(left, right, Tuple::tuple);
     }
 
-    /**
-     * Zip two streams into one using a {@link BiFunction} to produce resulting values.
-     * <p>
-     * <code><pre>
-     * // ("1:a", "2:b", "3:c")
-     * Seq.of(1, 2, 3).zip(Seq.of("a", "b", "c"), (i, s) -> i + ":" + s)
-     * </pre></code>
-     */
     static <T1, T2, R> Sequence<R> zip(Stream<T1> left, Stream<T2> right, BiFunction<T1, T2, R> zipper) {
         final Iterator<T1> it1 = left.iterator();
         final Iterator<T2> it2 = right.iterator();
@@ -529,14 +336,6 @@ public interface Sequence<T> extends Stream<T>, Iterable<T> {
         return sequence(new Zip());
     }
 
-    /**
-     * Fold a stream to the left.
-     * <p>
-     * <code><pre>
-     * // "abc"
-     * Seq.of("a", "b", "c").foldLeft("", (u, t) -> u + t)
-     * </pre></code>
-     */
     static <T, U> U foldLeft(Stream<T> stream, U seed, BiFunction<U, ? super T, U> function) {
         final Iterator<T> it = stream.iterator();
         U result = seed;
@@ -551,14 +350,6 @@ public interface Sequence<T> extends Stream<T>, Iterable<T> {
         return findFirst(e -> java.util.Objects.equals(e, element)).isPresent();
     }
 
-    /**
-     * Fold a stream to the right.
-     * <p>
-     * <code><pre>
-     * // "cba"
-     * Seq.of("a", "b", "c").foldRight("", (t, u) -> u + t)
-     * </pre></code>
-     */
     static <T, U> U foldRight(Stream<T> stream, U seed, BiFunction<? super T, U, U> function) {
         return sequence(stream).reverse().foldLeft(seed, (u, t) -> function.apply(t, u));
     }
@@ -669,49 +460,26 @@ public interface Sequence<T> extends Stream<T>, Iterable<T> {
         return stream.map(Objects::toString).collect(Collectors.joining(separator));
     }
 
-    /**
-     * Collect a Stream into a List.
-     */
     static <T, C extends Collection<T>> C toCollection(Stream<T> stream, Supplier<C> collectionFactory) {
         return stream.collect(Collectors.toCollection(collectionFactory));
     }
 
-    /**
-     * Collect a Stream into a List.
-     */
     static <T> List<T> toList(Stream<T> stream) {
         return stream.collect(Collectors.toList());
     }
 
-    /**
-     * Collect a Stream into a Set.
-     */
     static <T> Set<T> toSet(Stream<T> stream) {
         return stream.collect(Collectors.toSet());
     }
 
-    /**
-     * Collect a Stream of {@link Tuple2} into a Map.
-     */
     static <T, K, V> Map<K, V> toMap(Stream<Tuple2<K, V>> stream) {
         return stream.collect(Collectors.toMap(Tuple2::v1, Tuple2::v2));
     }
 
-    /**
-     * Collect a Stream into a Map.
-     */
     static <T, K, V> Map<K, V> toMap(Stream<T> stream, Function<T, K> keyMapper, Function<T, V> valueMapper) {
         return stream.collect(Collectors.toMap(keyMapper, valueMapper));
     }
 
-    /**
-     * Returns a limited interval from a given Stream.
-     * <p>
-     * <code><pre>
-     * // (4, 5)
-     * Seq.of(1, 2, 3, 4, 5, 6).slice(3, 5)
-     * </pre></code>
-     */
     static <T> Sequence<T> slice(Stream<T> stream, long from, long to) {
         long f = Math.max(from, 0);
         long t = Math.max(to - f, 0);
@@ -719,26 +487,10 @@ public interface Sequence<T> extends Stream<T>, Iterable<T> {
         return sequence(stream.skip(f).limit(t));
     }
 
-    /**
-     * Returns a stream with n elements skipped.
-     * <p>
-     * <code><pre>
-     * // (4, 5, 6)
-     * Seq.of(1, 2, 3, 4, 5, 6).skip(3)
-     * </pre></code>
-     */
     static <T> Sequence<T> skip(Stream<T> stream, long elements) {
         return sequence(stream.skip(elements));
     }
 
-    /**
-     * Returns a stream with all elements skipped for which a predicate evaluates to <code>true</code>.
-     * <p>
-     * <code><pre>
-     * // (3, 4, 5)
-     * Seq.of(1, 2, 3, 4, 5).skipWhile(i -> i < 3)
-     * </pre></code>
-     */
     static <T> Sequence<T> skipWhile(Stream<T> stream, Predicate<? super T> predicate) {
         return skipUntil(stream, predicate.negate());
     }
